73510b65d28196cf7538b489c742071595d27e0f
Илья <makomck98@gmail.com>
2021-12-31 00:32:58 +0300

    Готово

--- src/components/services/api.ts
+++ src/components/services/api.ts
@@ -1,16 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
-
-const BACKEND_URL = 'https://8.react.pages.academy/wtw';
-const REQUEST_TIMEOUT = 5000;
-
-
-
 export const createAPI = (): AxiosInstance => {
-    const api = axios.create({
-        baseURL: BACKEND_URL,
-        timeout: REQUEST_TIMEOUT,
-    });
+    const api = axios.create();
 
     api.interceptors.response.use(
         (response: AxiosResponse) => response,
